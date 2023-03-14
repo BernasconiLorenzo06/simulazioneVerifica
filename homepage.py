4c9b84d3f27afe6aa39ca1ba5962f8748b4c7ce4
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2023-03-14 11:55:14 +0000

    passaggio parametro

--- homepage.py
+++ homepage.py
@@ -12,17 +12,9 @@ def home():
 def mappa():
     return render_template('mappa.html')
 
-@app.route('/mappa600', methods=['GET'])
-def mappa600():
-    return render_template('mappa.html',larghezza = 600)
-
-@app.route('/mappa800', methods=['GET'])
-def mappa800():
-    return render_template('mappa.html', larghezza =  800)
-
-@app.route('/mappa1000', methods=['GET'])
-def mappa1000():
-    return render_template('mappa.html',larghezza =  1000)
+@app.route('/mappa/<width>', methods=['GET'])
+def mappaWidth(width):
+    return render_template('mappa.html',larghezza = width)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
